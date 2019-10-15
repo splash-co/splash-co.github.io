05798b961a4c14d513c9a1626fca70159da8560a
Guilherme Almeida <noghartt@outlook.com>
2019-10-15 00:18:26 -0300

    Switch and cases errors on form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(() => {
   $('.input--telephone').mask('(00) 90000-0000')
-})
+})  
+
+function validateKeyStrokes(event) {
+  var charCode = (event.which) ? event.which : event.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return true
+  }
+  return false
+}
 
 const changeValueButton = (signal, check) => {
   if (check) {
@@ -41,8 +49,6 @@ const fetchData = () => {
     maxPrice: document.querySelector('.input--maxPrice').value
   }
 
-  const mainDate = (new Date()).getTime()
-
   return (
     fetch(api, {
       method: 'POST',
@@ -66,14 +72,59 @@ const fetchData = () => {
         })
       }
     })
-    .catch(err => {     
-      const error =  {err}
+    .catch(err => {
+      const inputName = document.querySelector('.input--name'),
+        inputEmail = document.querySelector('.input--email'),
+        inputTelephone = document.querySelector('.input--telephone')
+
+      const error = {err}
+      console.log(error.err.message)
 
       switch (error.err.message) {
         case '"fullName" is not allowed to be empty':
-          console.log('banaan')
-          const input = document.querySelector('.input--name')
-          break
+          inputName.className = 'form__element--input--error input--name'
+          inputName.placeholder = 'O campo está vazio. Favor inserir um nome.'
+          inputName.onblur = function(e) {e.target.placeholder = 'O campo está vazio. Favor inserir um nome.'}
+          inputName.onfocus = function(e) {e.target.placeholder = ''}
+        break
+
+        case '"fullName" length must be at least 2 characters long':
+          inputName.className = 'form__element--input--error input--name'
+          inputName.value = ''
+          inputName.placeholder = 'O campo precisa de mais de dois caracteres.'
+          inputName.onblur = function(e) {e.target.placeholder = 'O campo precisa de mais de dois caracteres.'}
+          inputName.onfocus = function(e) {e.target.placeholder = ''}
+        break
+
+        case '"email" is not allowed to be empty':
+          inputEmail.className = 'form__element--input--error input--email'
+          inputEmail.placeholder = 'O campo está vazio. Favor insira um email.'
+          inputEmail.onblur = function(e) {e.target.placeholder = 'O campo está vazio. Favor insira um email.'}
+          inputEmail.onfocus = function(e) {e.target.placeholder = ''}
+        break
+
+        case '"email" must be a valid email':
+          inputEmail.className = 'form__element--input--error input--email'
+          inputEmail.value = ''
+          inputEmail.placeholder = 'O e-mail está errado. Favor, insira um e-mail válido.'
+          inputEmail.onblur = function(e) {e.target.placeholder = 'O e-mail está errado. Favor insira um e-mail válido.'}
+          inputEmail.onfocus = function(e) {e.target.placeholder = ''}
+        break
+
+        case '"phone" is not allowed to be empty':
+          inputTelephone.className = 'form__element--input--error input--telephone'
+          inputTelephone.placeholder = 'O campo está vazio. Favor, insira um telefone.'
+          inputTelephone.onblur = function(e) {e.target.placeholder = 'O campo está vazio. Favor insira um telefone.'}
+          inputTelephone.onfocus = function(e) {e.target.placeholder = ''}
+        break
+
+        case '"phone" with value "12321" fails to match the required pattern: /^[0-9]{8,13}$/':
+          inputTelephone.className = 'form__element--input--error input--telephone'
+          inputTelephone.placeholder = 'O campo está incorreto. Favor, insira um telefone correto.'
+          inputTelephone.value = ''
+          inputTelephone.onblur = function(e) {e.target.placeholder = 'O campo está incorreto. Favor, insira um telefone correto.'}
+          inputTelephone.onfocus = function(e) {e.target.placeholder = ''}
+        break
       }
     })
   )
