ae13b6f8ab9a02050c036d293d4f5ff6044e89f1
Matheus Souza <matheu.sk.9@hotmail.com>
2019-10-15 18:20:44 -0300

    Validate fullname input

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,38 +1,46 @@
 $(document).ready(() => {
-  $('.input--telephone').mask('(00) 90000-0000')
-})  
+  $('.input--telephone').mask('(00) 90000-0000');
+});
 
 function validateKeyStrokes(event) {
-  var charCode = (event.which) ? event.which : event.keyCode
+  var charCode = event.which ? event.which : event.keyCode;
   if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
 
 const changeValueButton = (signal, check) => {
   if (check) {
-    let inputValue = parseInt(document.querySelector('.input__value--quantityDay').value)
+    let inputValue = parseInt(
+      document.querySelector('.input__value--quantityDay').value
+    );
 
-    inputValue = signal ? inputValue + 1 : inputValue - 1
-  
-    if (inputValue <= 0 ) {
-      return inputValue = 1
+    inputValue = signal ? inputValue + 1 : inputValue - 1;
+
+    if (inputValue <= 0) {
+      return (inputValue = 1);
     }
 
-    document.querySelector('.input__value--quantityDay').value = `${inputValue}`
+    document.querySelector(
+      '.input__value--quantityDay'
+    ).value = `${inputValue}`;
   } else {
-    let inputValue = parseInt(document.querySelector('.input__value--quantityPeople').value)
+    let inputValue = parseInt(
+      document.querySelector('.input__value--quantityPeople').value
+    );
 
-    inputValue = signal ? inputValue + 1 : inputValue - 1
-  
-    if (inputValue <= 1 ) {
-      return inputValue = 2
+    inputValue = signal ? inputValue + 1 : inputValue - 1;
+
+    if (inputValue <= 1) {
+      return (inputValue = 2);
     }
 
-    document.querySelector('.input__value--quantityPeople').value = `${inputValue}`
+    document.querySelector(
+      '.input__value--quantityPeople'
+    ).value = `${inputValue}`;
   }
-}
+};
 
 const inputName = document.querySelector('.input--name'),
   inputEmail = document.querySelector('.input--email'),
@@ -40,14 +48,12 @@ const inputName = document.querySelector('.input--name'),
   inputDate = document.querySelector('.input--date'),
   inputCity = document.querySelector('.input--city'),
   inputMinPrice = document.querySelector('.input--minPrice'),
-  inputMaxPrice = document.querySelector('.input--maxPrice')
+  inputMaxPrice = document.querySelector('.input--maxPrice');
 
-const checkValues = (className) => {
-
-}
+const checkValues = className => {};
 
 const fetchData = () => {
-  const api = 'https://splashco.herokuapp.com/api/landing/submit'
+  const api = 'https://splashco.herokuapp.com/api/landing/submit';
 
   const bodyProperty = {
     fullName: inputName.value,
@@ -59,7 +65,7 @@ const fetchData = () => {
     numDays: document.querySelector('.input__value--quantityDay').value,
     minPrice: inputMinPrice.value,
     maxPrice: inputMaxPrice.value
-  }
+  };
 
   // return (
   //   fetch(api, {
@@ -82,4 +88,13 @@ const fetchData = () => {
   //     }
   //   })
   // )
-}
+};
+
+const validateFullname = e => {
+  if (e.target.value.length < 2) {
+    document
+      .querySelector('.input--name')
+      .classList.add('form__element--input--error');
+    document.querySelector('.form__element--finish__button').disabled = true;
+  }
+};
