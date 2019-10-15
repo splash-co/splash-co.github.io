4a17241057d3f94fbefb7c7f667f17dcd191e54b
Matheus Souza <matheu.sk.9@hotmail.com>
2019-10-15 12:14:05 -0300

    Handle api responde on UI

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,33 +1,41 @@
 $(document).ready(() => {
-  $('.input--telephone').mask('(00) 90000-0000')
-})
+  $('.input--telephone').mask('(00) 90000-0000');
+});
 
 const changeValueButton = (signal, check) => {
   if (check) {
-    let inputValue = parseInt(document.querySelector('.input__value--quantityDay').value)
+    let inputValue = parseInt(
+      document.querySelector('.input__value--quantityDay').value
+    );
 
-    inputValue = signal ? inputValue + 1 : inputValue - 1
-  
-    if (inputValue <= 0 ) {
-      return inputValue = 1
+    inputValue = signal ? inputValue + 1 : inputValue - 1;
+
+    if (inputValue <= 0) {
+      return (inputValue = 1);
     }
 
-    document.querySelector('.input__value--quantityDay').value = `${inputValue}`
+    document.querySelector(
+      '.input__value--quantityDay'
+    ).value = `${inputValue}`;
   } else {
-    let inputValue = parseInt(document.querySelector('.input__value--quantityPeople').value)
+    let inputValue = parseInt(
+      document.querySelector('.input__value--quantityPeople').value
+    );
 
-    inputValue = signal ? inputValue + 1 : inputValue - 1
-  
-    if (inputValue <= 1 ) {
-      return inputValue = 2
+    inputValue = signal ? inputValue + 1 : inputValue - 1;
+
+    if (inputValue <= 1) {
+      return (inputValue = 2);
     }
 
-    document.querySelector('.input__value--quantityPeople').value = `${inputValue}`
+    document.querySelector(
+      '.input__value--quantityPeople'
+    ).value = `${inputValue}`;
   }
-}
+};
 
 const fetchData = () => {
-  const api = 'https://splashco.herokuapp.com/api/landing/submit'
+  const api = 'https://splashco.herokuapp.com/api/landing/submit';
 
   const bodyProperty = {
     fullName: document.querySelector('.input--name').value,
@@ -39,42 +47,41 @@ const fetchData = () => {
     numDays: document.querySelector('.input__value--quantityDay').value,
     minPrice: document.querySelector('.input--minPrice').value,
     maxPrice: document.querySelector('.input--maxPrice').value
-  }
+  };
 
-  const mainDate = (new Date()).getTime()
+  const modal = document.querySelector('.container__confirm--submit');
+  const modalText = document.querySelector('.container__confirm--submit p');
 
-  return (
-    fetch(api, {
-      method: 'POST',
-      body: JSON.stringify(bodyProperty),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
+  modalText.style.textAlign = 'center';
+  modal.classList.toggle('none');
+  document.body.style.overflow = 'hidden';
+
+  return fetch(api, {
+    method: 'POST',
+    body: JSON.stringify(bodyProperty),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
     .then(res => {
-      if(res.ok) {
-        const modal = document.querySelector('.container__confirm--submit')
-
-        modal.style.display = 'flex'
-        document.body.style.overflow = 'hidden'
-
-        return res.json()
+      if (res.ok) {
+        modal.classList.toggle('none');
+        document.querySelector('#success').classList.toggle('none');
       } else {
-        return res.text().then(text => {
-          const error = JSON.parse(text)
-          throw new Error(error.message)
-        })
+        modal.classList.add('none');
+        document.querySelector('#failure').classList.toggle('none');
       }
     })
-    .catch(err => {     
-      const error =  {err}
+    .catch(err => {
+      const error = { err };
 
       switch (error.err.message) {
         case '"fullName" is not allowed to be empty':
-          console.log('banaan')
-          const input = document.querySelector('.input--name')
-          break
+          console.log('banaan');
+          const input = document.querySelector('.input--name');
+          break;
       }
-    })
-  )
-}
+    });
+};
+
+const refreshPage = () => window.location.reload();
