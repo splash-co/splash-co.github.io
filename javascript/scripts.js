2b6fee08bcb41be92b67ce287d4a082ec45b5bd4
Matheus Souza <matheu.sk.9@hotmail.com>
2019-10-25 13:17:33 -0300

    Validate some inputs

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -22,8 +22,9 @@ const changeValueButton = (signal, check) => {
       return (inputValue = 1);
     }
 
-    document.querySelector('.input__value--quantityDay').value = `${inputValue}`;
-
+    document.querySelector(
+      '.input__value--quantityDay'
+    ).value = `${inputValue}`;
   } else {
     let inputValue = parseInt(
       document.querySelector('.input__value--quantityPeople').value
@@ -35,7 +36,9 @@ const changeValueButton = (signal, check) => {
       return (inputValue = 2);
     }
 
-    document.querySelector('.input__value--quantityPeople').value = `${inputValue}`;
+    document.querySelector(
+      '.input__value--quantityPeople'
+    ).value = `${inputValue}`;
   }
 };
 
@@ -45,7 +48,7 @@ const fetchData = () => {
   const api = 'https://splashco.herokuapp.com/api/landing/submit';
 
   const bodyProperty = {
-    fullName: document.querySelector('.input--name').value,
+    fullName: document.querySelector('.input--name'),
     email: document.querySelector('.input--email').value,
     phone: $('.input--telephone').cleanVal(),
     city: document.querySelector('.select--city').value,
@@ -54,15 +57,59 @@ const fetchData = () => {
     numDays: document.querySelector('.input__value--quantityDay').value,
     minPrice: document.querySelector('.input--minPrice').value,
     maxPrice: document.querySelector('.input--maxPrice').value
-  }
+  };
 };
 
-const validateFullname = (evt) => {
-  if (evt.target.value.length < 2) {
+const validateFullname = evt => {
+  if (evt.target.value.length < 2 || !evt.target.value.match(/^[A-Za-z]+$/)) {
     document
       .querySelector('.input--name')
-      .classList.add('form__element--input--error')
+      .classList.add('form__element--input--error');
 
-      document.querySelector('.form__element--finish__button').disabled = true
+    document.querySelector('.form__element--finish__button').disabled = true;
+  } else {
+    document
+      .querySelector('.input--name')
+      .classList.remove('form__element--input--error');
+
+    document.querySelector('.form__element--finish__button').disabled = false;
   }
 };
+
+const validateEmail = evt => {
+  if (
+    !evt.target.value.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    document
+      .querySelector('.input--email')
+      .classList.add('form__element--input--error');
+
+    document.querySelector('.form__element--finish__button').disabled = true;
+  } else {
+    document
+      .querySelector('.input--email')
+      .classList.remove('form__element--input--error');
+
+    document.querySelector('.form__element--finish__button').disabled = false;
+  }
+};
+
+const validatePhone = evt => {
+  if (!evt.target.value.match(/^[(]{0,1}[0-9]{2}[)]{0,1}[-\s0-9]{8,13}$/)) {
+    document
+      .querySelector('.input--telephone')
+      .classList.add('form__element--input--error');
+
+    document.querySelector('.form__element--finish__button').disabled = true;
+  } else {
+    document
+      .querySelector('.input--telephone')
+      .classList.remove('form__element--input--error');
+
+    document.querySelector('.form__element--finish__button').disabled = false;
+  }
+};
+
+
